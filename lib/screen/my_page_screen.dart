4f9577037129a5f0f736ec1020bb25e05a1dd218
SubinJang <jsb408@gmail.com>
2021-06-16 22:37:38 +0900

    refactor: Adjust MVVM to My Page

--- lib/screen/my_page_screen.dart
+++ lib/screen/my_page_screen.dart
@@ -1,246 +1,138 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hoxy/model/member.dart';
+import 'package:get/get.dart';
 import 'package:hoxy/screen/ban_list_screen.dart';
-import 'package:hoxy/screen/login_screen.dart';
 import 'package:hoxy/service/emoji_service.dart';
-import 'package:hoxy/service/location_service.dart';
-import 'package:hoxy/view/alert_platform_dialog.dart';
 import 'package:hoxy/view/background_button.dart';
 import 'package:hoxy/view/grade_button.dart';
 import 'package:hoxy/view/my_page_element.dart';
-import 'package:hoxy/service/loading.dart';
+import 'package:hoxy/viewmodel/my_page_view_model.dart';
 
 import '../constants.dart';
 
-class MyPageScreen extends StatefulWidget {
-  @override
-  _MyPageScreenState createState() => _MyPageScreenState();
-}
-
-class _MyPageScreenState extends State<MyPageScreen> {
-  String _currentTownName = LocationService.townName;
-  late String _myTownName;
-
-  showEmojiDialog(String emoji) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertPlatformDialog(
-        title: Text('이모지 변경'),
-        content: Text(emoji, style: TextStyle(fontSize: 40)),
-        children: [
-          AlertPlatformDialogButton(
-            child: Text('적용'),
-            onPressed: () async {
-              Loading.show();
-              await kFirestore.collection('member').doc(kAuth.currentUser?.uid).update({'emoji': emoji});
-              Loading.dismiss();
-            },
-          ),
-          AlertPlatformDialogButton(
-            child: Text('재시도'),
-            onPressed: () {
-              showEmojiDialog(EmojiService.randomEmoji());
-            },
-          ),
-          AlertPlatformDialogButton(
-            child: Text('취소'),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
-  }
-
+class MyPageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('마이페이지'),
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertPlatformDialog(
-                  title: Text('로그아웃'),
-                  content: Text('로그아웃하시겠습니까?'),
-                  children: [
-                    AlertPlatformDialogButton(child: Text('아니오'), onPressed: () {}),
-                    AlertPlatformDialogButton(
-                      child: Text('네'),
-                      onPressed: () {
-                        kAuth.signOut();
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-                      },
-                    ),
-                  ],
-                ),
-              );
-            },
-          )
-        ],
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: kFirestore.collection('member').doc(kAuth.currentUser?.uid).snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          Member user = Member.from(snapshot.data!);
-          _myTownName = user.town;
-
-          return Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 12, right: 12, left: 12),
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
+    return GetBuilder<MyPageViewModel>(
+      init: MyPageViewModel(),
+      builder: (_viewModel) => Scaffold(
+        appBar: AppBar(
+          title: Text('마이페이지'),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () => _viewModel.signOut()
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 12, right: 12, left: 12),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            _viewModel.user.emoji,
+                            style: TextStyle(fontSize: 60),
+                          ),
+                          Container(
+                            width: 42,
+                            height: 24,
+                            child: BackgroundButton(
+                              title: '변경',
+                              textStyle: TextStyle(fontSize: 8, color: Colors.white),
+                              color: kDisabledColor,
+                              onPressed: () => _viewModel.showEmojiDialog(EmojiService.randomEmoji()),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: Column(
                           mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             Text(
-                              user.emoji,
-                              style: TextStyle(fontSize: 60),
+                              '${_viewModel.user.email} 님의\n인연지수는',
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                             Container(
-                              width: 42,
-                              height: 24,
-                              child: BackgroundButton(
-                                title: '변경',
-                                textStyle: TextStyle(fontSize: 8, color: Colors.white),
-                                color: kDisabledColor,
-                                onPressed: () {
-                                  showEmojiDialog(EmojiService.randomEmoji());
-                                },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: LinearProgressIndicator(
+                                  minHeight: 16,
+                                  backgroundColor: kExpBackgroundColor,
+                                  valueColor: AlwaysStoppedAnimation<Color>(kExpValueColor),
+                                  value: _viewModel.user.exp / 100,
+                                ),
                               ),
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                GradeButton(birth: _viewModel.user.birth),
+                                SizedBox(width: 5),
+                                Text(
+                                  '동네대장🥳 입니다',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                )
+                              ],
                             ),
                           ],
                         ),
-                        SizedBox(width: 8),
-                        Expanded(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                '${user.email} 님의\n인연지수는',
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Container(
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: LinearProgressIndicator(
-                                    minHeight: 16,
-                                    backgroundColor: kExpBackgroundColor,
-                                    valueColor: AlwaysStoppedAnimation<Color>(kExpValueColor),
-                                    value: user.exp / 100,
-                                  ),
-                                ),
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  GradeButton(birth: user.birth),
-                                  SizedBox(width: 5),
-                                  Text(
-                                    '동네대장🥳 입니다',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    MyPageLocation(
-                      icon: CupertinoIcons.map_pin_ellipse,
-                      title: '현재 동네',
-                      titleColor: Color(0xFFF58651),
-                      location: _currentTownName,
-                      onTap: () async {
-                        Loading.show();
-                        if (await LocationService.getCurrentLocation()) {
-                          setState(() {
-                            print(LocationService.townName);
-                            _currentTownName = LocationService.townName;
-                          });
-                          Loading.showSuccess('위치 변경 완료');
-                        } else
-                          Loading.showError('위치 읽기 실패');
-                      },
-                    ),
-                    MyPageLocation(
-                      icon: Icons.home_outlined,
-                      title: '우리 동네',
-                      location: _myTownName,
-                      onTap: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertPlatformDialog(
-                                title: Text('동네 변경'),
-                                content: Text('우리 동네를 현재 동네로 변경하시겠습니까?'),
-                                children: [
-                                  AlertPlatformDialogButton(child: Text('아니오'), onPressed: () {}),
-                                  AlertPlatformDialogButton(
-                                      child: Text('네'),
-                                      onPressed: () async {
-                                        Loading.show();
-                                        await kFirestore.collection('member').doc(kAuth.currentUser?.uid).update({
-                                          'city': LocationService.cityName,
-                                          'town': LocationService.townName,
-                                          'location': LocationService.geoPoint
-                                        }).catchError((error) {
-                                          print(error);
-                                          Loading.showError('변경 실패');
-                                        });
-                                        Loading.showSuccess('변경되었습니다');
-                                      })
-                                ],
-                              );
-                            });
-                      },
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  MyPageLocation(
+                    icon: CupertinoIcons.map_pin_ellipse,
+                    title: '현재 동네',
+                    titleColor: Color(0xFFF58651),
+                    location: _viewModel.currentTownName,
+                    onTap: () async => _viewModel.updateLocation(),
+                  ),
+                  MyPageLocation(
+                    icon: Icons.home_outlined,
+                    title: '우리 동네',
+                    location: _viewModel.myTownName,
+                    onTap: () => _viewModel.changeLocation()
+                  ),
+                ],
               ),
-              SizedBox(height: 1),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    MyPageButton(
-                      icon: CupertinoIcons.person_crop_circle_badge_xmark,
-                      title: '만남거부목록',
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => BanListScreen()));
-                      },
-                    ),
-                  ],
-                ),
+            ),
+            SizedBox(height: 1),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  MyPageButton(
+                    icon: CupertinoIcons.person_crop_circle_badge_xmark,
+                    title: '만남거부목록',
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => BanListScreen())),
+                  ),
+                ],
               ),
-            ],
-          );
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/viewmodel/my_page_view_model.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hoxy/model/member.dart';
+import 'package:hoxy/screen/login_screen.dart';
+import 'package:hoxy/service/emoji_service.dart';
+import 'package:hoxy/service/loading.dart';
+import 'package:hoxy/service/location_service.dart';
+import 'package:hoxy/view/alert_platform_dialog.dart';
+
+import '../constants.dart';
+
+class MyPageViewModel extends GetxController {
+  String _currentTownName = LocationService.townName;
+  String get currentTownName => _currentTownName;
+
+  late String _myTownName;
+  String get myTownName => _myTownName;
+
+  Member _user = Member();
+  Member get user => _user;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    kFirestore.collection('member').doc(kAuth.currentUser?.uid).snapshots().listen((event) {
+      _user = Member.from(event);
+      _myTownName = _user.town;
+      update();
+    });
+  }
+
+  void showEmojiDialog(String emoji) {
+    Get.dialog(
+      AlertPlatformDialog(
+        title: Text('이모지 변경'),
+        content: Text(emoji, style: TextStyle(fontSize: 40)),
+        children: [
+          AlertPlatformDialogButton(
+            child: Text('적용'),
+            onPressed: () async {
+              Loading.show();
+              await kFirestore.collection('member').doc(kAuth.currentUser?.uid).update({'emoji': emoji});
+              Loading.dismiss();
+            },
+          ),
+          AlertPlatformDialogButton(
+            child: Text('재시도'),
+            onPressed: () {
+              showEmojiDialog(EmojiService.randomEmoji());
+            },
+          ),
+          AlertPlatformDialogButton(
+            child: Text('취소'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateLocation() async {
+    Loading.show();
+    if (await LocationService.getCurrentLocation()) {
+      print(LocationService.townName);
+      _currentTownName = LocationService.townName;
+      update();
+      Loading.showSuccess('위치 변경 완료');
+    } else Loading.showError('위치 읽기 실패');
+  }
+
+  void changeLocation() async {
+    Get.dialog(
+        AlertPlatformDialog(
+            title: Text('동네 변경'),
+            content: Text('우리 동네를 현재 동네로 변경하시겠습니까?'),
+            children: [
+              AlertPlatformDialogButton(child: Text('아니오'), onPressed: () {}),
+              AlertPlatformDialogButton(
+                  child: Text('네'),
+                  onPressed: () async {
+                    Loading.show();
+                    await kFirestore
+                        .collection('member')
+                        .doc(kAuth.currentUser?.uid)
+                        .update({
+                      'city': LocationService.cityName,
+                      'town': LocationService.townName,
+                      'location': LocationService.geoPoint
+                    }).catchError((error) {
+                      print(error);
+                      Loading.showError('변경 실패');
+                    });
+                    Loading.showSuccess('변경되었습니다');
+                  })
+            ],
+        ));
+  }
+
+  void signOut() {
+    Get.dialog(AlertPlatformDialog(
+        title: Text('로그아웃'),
+        content: Text('로그아웃하시겠습니까?'),
+        children: [
+          AlertPlatformDialogButton(child: Text('아니오'), onPressed: () {}),
+          AlertPlatformDialogButton(
+            child: Text('네'),
+            onPressed: () {
+              kAuth.signOut();
+              Get.off(LoginScreen());
+            },
+          ),
+        ],
+      ));
+  }
+}
